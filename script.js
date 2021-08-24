71f346a4141fb138c260ac047389564347a1ba2c
JSoorajKrishna <83532645+JSoorajKrishna@users.noreply.github.com>
2021-08-24 19:49:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ for(let i=1;i<17;i++)
     keys[i].onclick = function(){
     let nota = keys[i].getAttribute("id");
     if(clicked)
-        {
+        {if(i==6)
+            i=5;
+        if(i==14)
+            i=13;
         console.log(i);
         embed.setAttribute("src",`notes/${i}.mp3`);
         clicked = false;
