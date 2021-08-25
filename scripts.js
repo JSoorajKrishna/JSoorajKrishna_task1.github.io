a7ed5ef909fabfdb164bd7ebafaedd86c993658d
JSoorajKrishna <83532645+JSoorajKrishna@users.noreply.github.com>
2021-08-25 09:14:33 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let keys = document.querySelectorAll("div");
-let embed = document.querySelector("embed");
-
-for(let i=1;i<15;i++)
-{
-    keys[i].onclick = function(){
-        embed.setAttribute("src",`notes/${i}.mp3`);
-    }
-}
